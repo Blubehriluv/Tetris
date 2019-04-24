5319b8d23151bafd22738bc8fb000e5d9ba0a78a
Taylor McKenzie <taymcken@uat.edu>
2019-04-24 06:23:40 -0700

    Main Menu Design Complete
    Has buttons and UI

---
+++ Totres/Assets/Scripts/playGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class playGame : MonoBehaviour {
+
+    // Use this for initialization
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+}
