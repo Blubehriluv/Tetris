97a9c6baa46163d2c335af745f0e5e6f5434548d
Taylor McKenzie <taymcken@uat.edu>
2019-04-23 19:58:49 -0700

    Block Falling Perfected
    BlockFall script added

--- Totres/Assets/Scripts/BlockFall.cs
+++ Totres/Assets/Scripts/BlockFall.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlockFall : MonoBehaviour {
+public class BlockFall : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            transform.Translate(0, -1, 0);
+        }
+    }
 }
